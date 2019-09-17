cf2a095dee061eccc38c23df664709b2193471a1
jeisux92 <jei.sum41@gmail.com>
2019-09-17 16:52:03 -0500

    upload files

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+import 'src/app.dart';
+
+
+void main() {
+  runApp(new MyApp());
+}
+
+
---
+++ lib/src/app.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(home: Center(child: Text("Hola world")));
+  }
+}
